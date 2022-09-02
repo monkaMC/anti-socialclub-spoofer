c7b81b9bf231dab59ec0d4e1519f7c7c4b008412
Jamie <93123683+monkaMC@users.noreply.github.com>
2022-09-02 19:11:58 +0200

    Update index.js
    credits to @soren-s

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 const axios = require('axios');
-const secret = require('./config.json');
 
-function checkUser(name, id)
-{
-   // you getting the "secret id" when u join the patreon from the api creator.
-   // more informations at https://discord.gg/UgwaQzX5Tr
-   axios.get(`https://rid.itsdevil.com/api?id=${secret.id}&search=${name}`)
-        .then(response => 
-        {
-            var scid = response.data.rockstarAccount.rockstarId;
-            if(scid != id) return true;
-        })
-        .catch(error => 
-        {
-            console.log('Error: ', error.response.data);
-            return true;
-        })
+class AntiSpoof {
+    sqlKeywords = ["SELECT", "UPDATE", "DELETE", "INSERT", "INSERT INTO", "CREATE DATABASE", "ALTER DATABASE", "CREATE TABLE", "ALTER TABLE", "DROP TABLE", "CREATE INDEX", "DROP INDEX"];
+    secretId = "your id";
+    constructor() {
+        mp.events.add('playerJoin', (player) => this.playerJoin(player))
+    }
 
-    return false;
+    playerJoin(player) {
+        if (!mp.players.exists(player)) return;
+        let containsSqlKeyword = false;
+
+        for (let i = 0; i < this.sqlKeywords.length; i++) {
+            if (player.socialClub.includes(this.sqlKeywords[i])) {
+                containsSqlKeyword = true;
+                break;
+            }
+        }
+
+        if (containsSqlKeyword) {
+            player.ban('sql injection');
+            return;
+        }
+
+        const isSpoofing = this.checkUser(player.socialClub, player.rgscId) ? player.ban('spoofing') : false;
+
+    }
+
+    checkUser(name, id) {
+        axios.get(`https://rid.itsdevil.com/api?id=${this.secretId}&search=${name}`)
+            .then(response => {
+                var scid = response.data.rockstarAccount.rockstarId;
+                if (scid != id) return true;
+            })
+            .catch(error => {
+                console.log('Error: ', error.response.data);
+                return true;
+            })
+
+        return false;
+    }
 }
 
-mp.events.add('playerJoin', async (player) => {
-    if(player.socialClub.includes("SELECT" && "UPDATE" && "DELETE" && "INSERT" && "INSERT INTO" && "CREATE DATABASE" && "ALTER DATABASE" && "CREATE TABLE" && "ALTER TABLE" && "DROP TABLE" && "CREATE INDEX" && "DROP INDEX"))
-    {
-        // chance to 99% that the user is doing social club name spoofing and trying to execute sql code
-        if(spoofed)
-        {
-            player.notify(`cooler versuch`);
-            setTimeout(() => {
-                player.ban('spoofing.');
-            }, 5000);
-        }
-    }
-
-    var spoofed = await checkUser(player.socialClub, player.rgscId);
-
-    // chance to 99% that the user is doing social club id spoofing
-    if(spoofed)
-    {
-        player.notify(`cooler versuch`);
-        setTimeout(() => {
-            player.ban('spoofing.');
-        }, 5000);
-    }
-});
+new AntiSpoof();
